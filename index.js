f7eb5defa5a3f57525531a479a04553329da8d95
Gangjisu <51405284+Gangjisu@users.noreply.github.com>
2024-11-13 02:43:06 +0000

    download test 4

--- index.js
+++ index.js
@@ -34,14 +34,31 @@ function Toggle(button_id, content_id) {
 }
 
 //파일 다운로드
-function download_file(filename) {
-    // const filename = 'example.txt';
-    const apiUrl = '/fileDownload?filename=' + filename;
+const download_file = async () => {
+    // 단순 예시를 위해 filename을 하드코딩 하였으며, 함수의 파라미터로 받아 동적으로 사용할 수 있습니다.
+    const filename = '12d0cd704833e15b3d2478af9a709d93.jpg';
+    // GET 요청 보내기
+    fetch('http://localhost:8080/fileDownload?filename=' + filename, {
+        method: 'GET',
+    })
+    .then((response) => response.blob())
+    .then((blob) => {
+        // blob 객체를 통해 URL 생성
+        const url = window.URL.createObjectURL(blob);
+        const element = document.createElement('a');
 
-    const element = document.createElement('a');
-    element.href = apiUrl;
-    element.download = filename;
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
+        // 생성한 URL과 다운로드될 파일명 설정
+        element.setAttribute('href', url);
+        element.setAttribute('download', '다운로드될파일명.txt');
+
+        // 생성된 a tag를 body에 추가
+        document.body.appendChild(element);
+
+        // a tag 클릭을 통해 파일 다운로드 시도
+        element.click();
+
+        // 다운로드 후 a tag와 URL을 정리
+        element.parentNode.removeChild(element);
+        window.URL.revokeObjectURL(url);
+    });
 }
