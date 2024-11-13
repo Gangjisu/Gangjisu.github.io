3879e4d0103bff979265e39022f564e06282167c
Gangjisu <51405284+Gangjisu@users.noreply.github.com>
2024-11-13 01:55:53 +0000

    download test 2

--- index.js
+++ index.js
@@ -29,35 +29,18 @@ function Toggle(button_id, content_id) {
         toggleButton.style.backgroundColor = ''; //if input color in '' error
     }
 
-    const download_file = async (filename) => {
-        //const filename = 'example.txt';
-        
-        // GET 요청 보내기
-        fetch('http://localhost:8080/fileDownload?filename=' + filename, {
-            method: 'GET',
-        })
-        .then((response) => response.blob())
-        .then((blob) => {
-            // blob 객체를 통해 URL 생성
-            const url = window.URL.createObjectURL(blob);
-            const element = document.createElement('a');
+    //파일 다운로드
+    function download_file(filename) {
+        // const filename = 'example.txt';
+        const apiUrl = '/fileDownload?filename=' + filename;
 
-            // 생성한 URL과 다운로드될 파일명 설정
-            element.setAttribute('href', url);
-            element.setAttribute('download', '다운로드될파일명.txt');
-
-            // 생성된 a tag를 body에 추가
-            document.body.appendChild(element);
-
-            // a tag 클릭을 통해 파일 다운로드 시도
-            element.click();
-
-            // 다운로드 후 a tag와 URL을 정리
-            element.parentNode.removeChild(element);
-            window.URL.revokeObjectURL(url);
-        });
+        const element = document.createElement('a');
+        element.href = apiUrl;
+        element.download = filename;
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
     }
-
 
 }
 
